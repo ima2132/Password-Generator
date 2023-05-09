7456b6df9890fd0ae9b505af16e5a0235160e0ec
ima2132 <ima2132@columbia.edu>
2023-05-09 16:32:23 -0400

    created a form that meets the acceptance criteria

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-// Assignment code here
+// assignment code here
+
+// adding a form to the page 
+<form action="javascript:generatePassword()">
+<input type="number" id="length" placeholder="Password Length" min="8" max="128" />
+<input type="checkbox" id="lowercase" name="characters" value="lowercase" />
+<label for="lowercase">Lowercase</label>
+<input type="checkbox" id="uppercase" name="characters" value="uppercase" />
+<label for="uppercase">Uppercase</label>
+<input type="checkbox" id="numbers" name="characters" value="numbers" />
+<label for="numbers">Numbers</label>
+<input type="checkbox" id="symbols" name="characters" value="symbols" />
+<label for="symbols">Symbols</label>
+<input type="submit" value="Generate Password" />
+</form>
 
 
 // Get references to the #generate element
