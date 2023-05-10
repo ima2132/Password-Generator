ded501b2f4965f31b63fb8de008e36af7e9db646
ima2132 <ima2132@columbia.edu>
2023-05-09 20:44:45 -0400

    added console.log; removed const length document.querySelector with a prompt; fixed the error that kept recurring about being null

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const generateBtn = document.querySelector("#generate");
 generateBtn.addEventListener("click", () => {
   
 // make sure to get the user's input 
+const length = prompt("Please choose a password length between 8 and 128 characters.");
+
 const lowercase = confirm("Would you like to include lowercase characters?");
 
 const uppercase = confirm("Would you like to include uppercase characters?");
@@ -13,8 +15,6 @@ const uppercase = confirm("Would you like to include uppercase characters?");
 const numbers = confirm("Would you like to include numbers?");
 
 const special = confirm("Would you like to include special characters?");
-
-const length = parseInt(document.querySelector("#length").value, 10); 
 
 
 // make sure the user's input satisfies the given min-max character password length
@@ -31,24 +31,27 @@ if (length < 8 || length > 128) {
   // generating a password 
   const characters = [];
   if (lowercase) {
-    characters.push("abcdefghijklmnopqrstuvwxyz");
+    characters.push('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z');
   }
   if (uppercase) {
-    characters.push("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+    characters.push('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z');
   }
   if (numbers) {
-    characters.push("0123456789");
+    characters.push('0', '1', '2', '3', '4', '5', '6', '7', '8', '9');
   }
   if (special) {
-    characters.push("!@#$%^&*()-_+={}[]|\\:;'\",.<>/?");
+    characters.push('!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '{', '}', '[', ']', '|', '\\', ':', ';', '\'', '\"', ',', '.', '<', '>', '/', '?');
   }
 
+  console.log('characters:', characters);
+
   const password= [];
-  for (let i = 0; i < length; i++) {
+  for (let i = 0; i < 8; i++) {
     const character = characters[Math.floor(Math.random() * characters.length)];
     password.push(character);
   }
 
+  console.log('password:', password);
   // password input value
   document.querySelector("#password").value = password.join("");
 });
